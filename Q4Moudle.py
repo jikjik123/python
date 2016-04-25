9b4d397d78e400489771b2f5fab879deb68de552
jikjik123 <jikjik993@gmail.com>
2016-04-26 01:12:18 +0800

    Update Q4Moudle.py

--- Q4Moudle.py
+++ Q4Moudle.py
@@ -125,23 +125,3 @@ class UnorderedList:
             new_item = Node(item)
             new_item.next = next.next
             next.next = new_item
-if __name__ == "__main__":
-    mylist = UnorderedList()
-    print(mylist.isEmpty())
-    mylist.add(3323)
-    mylist.add('melon')
-    mylist.add(1024)
-    print(mylist.size())  # ['melon',3323,1024] This order may not be
-    mylist.append(555)
-    print(mylist.search(555))  # ['melon',3323,1024,555]
-    print(mylist.size())
-    mylist.remove(1024)  # ['melon',3323,555]
-    print(mylist.size())
-    print(mylist.index(555))
-    mylist.insert(2, 44)  # ['melon',3323,44,555]
-    print(mylist.index(44))
-    mylist.pop()  # ['melon',3323,44]
-    print(mylist.search(555))
-    mylist.pop(1)  # ['melon',44]
-    print(mylist.search(3323))
-
