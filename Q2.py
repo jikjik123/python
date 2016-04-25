80edefc9426bdb2dae3eb199e1c2f628c8409705
unknown <jikjik993@gmail.com>
2016-04-26 00:54:49 +0800

    Q2.py

---
+++ Q2.py
@@ -0,0 +1,10 @@
+Queue = [] #using list as a queue
+
+Queue.insert(0,'true') #enqueue of Queue
+print(Queue)
+Queue.insert(0,4) #enqueue of Queue
+print(Queue)
+size = len(Queue) #size of Queue
+print(size)
+Queue.pop() #dequeue of Queue
+print(Queue)
